a7e02ad14a291b9bc3fa33bc5403df9a98fbef85
Benjamin Ortiz <benortiz@ocf.berkeley.edu>
2013-10-06 00:22:56 -0700

    dynamic numbers and oct 5 for benjamin

---
+++ resources/js/designchallenge.js
@@ -0,0 +1,35 @@
+/*
+Count up from any date script-
+By JavaScript Kit (www.javascriptkit.com)
+Over 200+ free scripts here!
+*/
+
+
+
+function countup(yr,m,d){
+	var montharray=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+	var today=new Date();
+	var todayy=today.getYear();
+	var todaym=today.getMonth();
+	var todayd=today.getDate();
+
+	if (todayy < 1000){
+		todayy+=1900;
+	}
+
+	var todaystring=montharray[todaym]+" "+todayd+", "+todayy;
+	var paststring=montharray[m-1]+" "+d+", "+yr;
+	var difference=(Math.round((Date.parse(todaystring)-Date.parse(paststring))/(24*60*60*1000))*1)
+
+	return difference;
+}
+
+function addConverted() {
+	var header = document.getElementById('title');
+	var daysSince = countup(2013,10,01);
+	var wordDay = window.num2str.convert(daysSince);
+
+	header.innerHTML = "<span class=\"top\">" +wordDay+ " day</span><br /><span class=\"bottom\">design challenge.</span>";
+}
+
+window.onload = addConverted;
